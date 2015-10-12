e4eedd49eec7b7636c116ec10f4bf5ec7d198482
unknown <hbarre03@CSPC128.dcs.bbk.ac.uk>
2015-10-12 16:32:34 +0100

    changed variable 'paper' to 'p' ex3

--- Day 5/Ex3.groovy
+++ Day 5/Ex3.groovy
@@ -1,13 +1,16 @@
 //----------- paper size
+String p;
 boolean finished = false;
 while (!finished) {
 	print ">: ";
-	String paper = System.console().readLine();
+	p = System.console().readLine();
 	
-	char ch = paper.charAt(0) 
+	char ch = p.charAt(0) 
 	if(ch=='A' || ch=='a')
 		finished = true;
 	else
 		println "Incorrect input, please begin with A";
 }
 
+String sizeString = p.substring(1, p.size()); //remove the 'A'
+int paperSize = Integer.parseInt(sizeString);
