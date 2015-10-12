d740306369393bbf0789e61fe0612e27c06d0402
unknown <hbarre03@CSPC128.dcs.bbk.ac.uk>
2015-10-12 16:10:56 +0100

    created input loop Ex3

---
+++ Day 5/Ex3.groovy
@@ -0,0 +1,13 @@
+//----------- paper size
+boolean finished = false;
+while (!finished) {
+	print ">: ";
+	String paper = System.console().readLine();
+	
+	char ch = paper.charAt(0) 
+	if(ch=='A' || ch=='a')
+		finished = true;
+	else
+		println "Incorrect input, please begin with A";
+}
+
