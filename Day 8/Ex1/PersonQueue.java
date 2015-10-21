d1d18d9ac9e9ba278ce212867cf67ebd53258e66
Hijiri BBK <hijiribarrett@gmail.com>
2015-10-21 17:10:53 +0100

    Sample code from bbk-moodle

--- Day 8/Ex1/PersonQueue.java
+++ Day 8/Ex1/PersonQueue.java
@@ -1,12 +1,12 @@
 public interface PersonQueue {
-/**
-* Adds another person to the queue.
-*/
-void insert(Person person);
-/**
-* Removes a person from the queue.
-*/
-Person retrieve();
+	/**
+	* Adds another person to the queue.
+	*/
+	void insert(Person person);
+	/**
+	* Removes a person from the queue.
+	*/
+	Person retrieve();
 
-void print();
+	void print();
 }
