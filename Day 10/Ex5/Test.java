de7c744547aee2e546c1a4e1f838a6b016e1316a
Hijiri BBK <hijiribarrett@gmail.com>
2015-10-28 16:35:09 +0000

    Changed Test.java to better test the implementation of burn() and play().

--- Day 10/Ex5/Test.java
+++ Day 10/Ex5/Test.java
@@ -1,10 +1,17 @@
 public class Test {
 	public static void main (String[] args) {
+		Test t = new Test();
 		Guitar g1 = new Guitar();
 		
-		g1.play();
 		g1.changeStrings();
-		g1.burn();
+		t.testPlay(g1);
+		t.testBurn(g1);
 		g1 = null;
+	}
+	private void testBurn(WoodenObject wood) {
+		wood.burn();
+	}
+	private void testPlay(MusicalInstrument music) {
+		music.play();
 	}
 }
